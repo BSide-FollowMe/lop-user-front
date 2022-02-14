7cf972dc579184e1d903c2217888d87f6b248c2d
korkt-kim <gnscott6@naver.com>
2022-02-15 03:03:02 +0900

    feat: craete my plant page - not empty

--- src/views/my-page/MyPlant/Index.vue
+++ src/views/my-page/MyPlant/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="inner-container">
     <section class="title">내가 저장한 식물</section>
-    <div v-if="plants.length > 0"></div>
+    <List v-if="plants.length > 0" :plants="plants" />
     <Empty v-else />
   </div>
 </template>
@@ -9,26 +9,72 @@
 <script lang="ts">
 import { defineComponent, PropType } from 'vue';
 import Empty from './Empty.vue';
+import List from './List.vue';
 
 export default defineComponent({
   props: {
     plants: {
       type: Array as PropType<
         {
-          img: string;
+          imgUrl: string;
           name: string;
+          like: boolean;
         }[]
       >,
-      default: () => [],
+      default: () => [{
+        imgUrl:'',
+        name:'1',
+        like:true
+      },{
+        imgUrl:'',
+        name:'2',
+        like:true
+      },{
+        imgUrl:'',
+        name:'3',
+        like:true
+      },{
+        imgUrl:'',
+        name:'4',
+        like:true
+      },{
+        imgUrl:'',
+        name:'5',
+        like:true
+      },{
+        imgUrl:'',
+        name:'6',
+        like:true
+      },{
+        imgUrl:'',
+        name:'7',
+        like:true
+      },{
+        imgUrl:'',
+        name:'8',
+        like:true
+      },{
+        imgUrl:'',
+        name:'9',
+        like:true
+      },{
+        imgUrl:'',
+        name:'10',
+        like:true
+      },{
+        imgUrl:'',
+        name:'11',
+        like:true
+      }],
     },
   },
-  components: { Empty },
+  components: { Empty, List },
 });
 </script>
 
 <style lang="scss" scoped>
 @import '@/styles/';
-.title{
+.title {
   font-weight: var(--font-weight-bold);
   font-size: 32px;
   line-height: 38px;
@@ -38,14 +84,12 @@ export default defineComponent({
   /* deepgreen/1-main */
 
   color: var(--primary-color-1);
-  margin-bottom:50px;
-  @include breakpoint-down-sm{
+  margin-bottom: 50px;
+  @include breakpoint-down-sm {
     font-size: 18px;
     line-height: 22px;
     letter-spacing: -0.01em;
-    margin-bottom:40px;
+    margin-bottom: 40px;
   }
-
 }
-
 </style>
---
+++ src/views/my-page/MyPlant/List.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="inner-container">
+    <div class="plant" v-for="(plant, index) in newPlants" :key="index">
+      <img class="image" :src="plant.imgUrl" />
+      <div class="info">
+        <span class="name">{{ plant.name }}</span>
+        <img
+          v-if="plant.like"
+          class="icon heart-fill-icon"
+          :src="FullHeartIcon"
+          @click="
+            toggleLike(index);
+            requestChangeLike(index);
+          "
+        />
+        <img
+          v-else
+          class="icon heart-empty-icon"
+          :src="EmptyHeartIcon"
+          @click="
+            toggleLike(index);
+            requestChangeLike(index);
+          "
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType, ref } from 'vue';
+import EmptyHeartIcon from '@/assets/icon/heart-empty.svg';
+import FullHeartIcon from '@/assets/icon/heart-fill.svg';
+import { debounce } from 'lodash';
+import { useStore } from 'vuex';
+export default defineComponent({
+  props: {
+    plants: {
+      type: Array as PropType<
+        {
+          imgUrl: string;
+          name: string;
+          like: boolean;
+        }[]
+      >,
+      default: () => [],
+    },
+  },
+  setup(props) {
+    const store = useStore();
+    const newPlants = ref([...props.plants]);
+    const toggleLike = (index: number) => {
+      newPlants.value[index].like = !newPlants.value[index].like;
+      if (newPlants.value[index].like) {
+        store.dispatch('snack/openSnack', { text: '내가 저장한 식물에 추가했어요!', link: '/', color: '#C9704C' });
+      } else {
+        store.dispatch('snack/openSnack', { text: '내가 저장한 식물에서 삭제했어요!', color: '#C9704C' });
+      }
+    };
+    const requestChangeLike = debounce((index: number) => {
+      console.log('turned into ' + newPlants.value[index].like);
+    }, 300);
+
+    return {
+      EmptyHeartIcon,
+      FullHeartIcon,
+      requestChangeLike,
+      toggleLike,
+      newPlants,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/';
+.inner-container {
+  background: #ffffff;
+  border-radius: 4px;
+  padding: 60px;
+  min-width:960px;
+  display: flex;
+  gap: 40px 32px;
+  flex-wrap:wrap;
+  @include breakpoint-down-sm{
+    min-width:320px;
+    gap:20px;
+    justify-content: space-between;
+    padding:20px;
+  }
+  
+}
+.plant {
+  display: inline-block;
+}
+.image {
+  width: 186px;
+  height: 186px;
+  @include breakpoint-down-sm{
+    width: 130px;
+    height: 130px;
+  }
+}
+.info {
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+}
+.icon {
+  cursor: pointer;
+}
+</style>
