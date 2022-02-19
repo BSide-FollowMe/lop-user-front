9461814cd18b866ff8331a94dcfaa77ab929f85b
korkt-kim <gnscott6@naver.com>
2022-02-20 04:47:58 +0900

    feat: create gradeGuide Modal

--- src/views/debug/ModalTest.vue
+++ src/views/debug/ModalTest.vue
@@ -7,37 +7,60 @@
     <br />
     <button @click="openModal('ContactUsModal')">문의하기 모달</button>
     <br />
-    <RequestModal v-if="isShow" :options="options" @close="isShow = false" />
+    <button @click="openModal('GradeGuideModal')">등급안내 모달</button>
+    <br />
+    <RequestModal v-if="isShow && options.type === 'Request'" :options="options" @close="isShow = false" />
+    <GradeGuideModal v-if="isShow && options.type === 'GradeGuide'" :options="options" @close="isShow = false" />
   </div>
 </template>
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
 import RequestModal from '@/components/modals/RequestModal.vue';
-const optionsSampleList: any = {
+import GradeGuideModal from '@/components/modals/GradeGuideModal.vue';
+interface ModalOption {
+  modalTitle: string;
+  objective?: string;
+  contentsLabel?: string;
+  type: string;
+}
+const optionsSampleList: {
+  ReportModal: ModalOption;
+  RegistPlantModal: ModalOption;
+  ContactUsModal: ModalOption;
+  GradeGuideModal: ModalOption; // todo: typescript Pick option 공부 후 세분화...
+} = {
   ReportModal: {
     modalTitle: '제보하기',
     objective: '녹영',
     contentsLabel: '잘못된 정보나 건의하고 싶으신 내용을 작성해주세요.',
+    type: 'Request',
   },
   RegistPlantModal: {
     modalTitle: '식물 등록 요청하기',
     contentsLabel: '더 자세히 알고 싶은 식물명을 알려주세요. ',
+    type: 'Request',
   },
   ContactUsModal: {
     modalTitle: '문의하기',
     contentsLabel: '궁금한 점이나 제안하고 싶은 사항을 작성해주세요.',
+    type: 'Request',
+  },
+  GradeGuideModal: {
+    modalTitle: '등급안내',
+    type: 'GradeGuide',
   },
 };
 export default defineComponent({
   name: 'ModalTest',
   components: {
     RequestModal,
+    GradeGuideModal,
   },
   setup() {
     const targetModal = ref('DefaultModal');
     const isShow = ref(false);
-    const options = ref({});
-    function openModal(targetComponentName: string) {
+    const options = ref({} as ModalOption);
+    function openModal(targetComponentName: 'ReportModal' | 'RegistPlantModal' | 'ContactUsModal' | 'GradeGuideModal') {
       isShow.value = true;
       options.value = optionsSampleList[targetComponentName];
     }
