70534ec533e4e657c659b483b208b7e0f7ee9975
korkt-kim <gnscott6@naver.com>
2022-02-22 00:28:37 +0900

    fix: remodel design of blight guide

--- src/views/guide/WaterGuide/Inspect.vue
+++ src/views/guide/WaterGuide/Inspect.vue
@@ -45,7 +45,7 @@ export default defineComponent({
 @import '@/styles';
 .main {
   &__title {
-    font-weight: var(--font--weight-bold);
+    font-weight: var(--font-weight-bold);
     font-size: 18px;
     line-height: 26px;
     /* identical to box height, or 144% */
@@ -59,13 +59,13 @@ export default defineComponent({
     }
   }
   &__content {
-    font-weight: var(--font--weight-medium);
+    font-weight: var(--font-weight-medium);
     font-size: 16px;
     line-height: 26px;
     /* or 162% */
     letter-spacing: -0.01em;
     /* text/2 */
-    color: var(----text-color-2);
+    color: var(--text-color-2);
     margin-bottom: 40px;
     @include breakpoint-down-sm {
       font-size: 14px;
@@ -111,13 +111,13 @@ export default defineComponent({
     }
   }
   &__content {
-    font-weight: var(--font--weight-medium);
+    font-weight: var(--font-weight-medium);
     font-size: 16px;
     line-height: 26px;
     /* or 162% */
     letter-spacing: -0.01em;
     /* text/2 */
-    color: var(----text-color-2);
+    color: var(--text-color-2);
     @include breakpoint-down-sm {
       font-size: 14px;
       line-height: 22px;
