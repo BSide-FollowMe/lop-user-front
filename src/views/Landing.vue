363eaf7174036718fd95b54af79df8ce6b6cef61
Haeun <42575126+SummerRiversound@users.noreply.github.com>
2022-03-14 22:36:36 +0900

    chore: 테스트 페이지 제거

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,44 +1,14 @@
 <template>
-  <div class="text-center">
-    <h1>식물의 언어 임시 라우팅 페이지</h1>
-    <router-link to="/home">메인페이지</router-link>
-    <br />
-    <router-link to="/signin">로그인/회원가입</router-link>
-    <br />
-    <router-link to="/search?q=콩난">검색결과</router-link>
-    <br />
-    <router-link to="/modal-test">모달테스트 페이지</router-link>
-    <br />
-    <router-link to="/qna">질문 답변 게시판</router-link>
-    <br/>
-    <router-link to="/qna/edit">질문 답변 글쓰기</router-link>
-    <br/>
-    <router-link to="/plant/45">식물상세</router-link>
-    <br />
-    <router-link to="/me">마이페이지</router-link>
-    <br />
-    <router-link to="/me/my-plant">내가저장한식물</router-link>
-    <br />
-    <router-link to="/me/setting">마이페이지-세팅</router-link>
-    <br />
-    <router-link to="/me/qna?list=questions">마이페이지-질문,답변</router-link>
-    <br />
-    <router-link to="/guide/water">가이드 물주기</router-link>
-    <br />
-    <router-link to="/guide/ventilation">가이드 통풍</router-link>
-    <br />
-    <router-link to="/guide/sunlight">가이드 양지/음지</router-link>
-    <br />
-    <router-link to="/guide/blight">가이드 아플 때</router-link>
-    <br />
-    <router-link to="/logout">로그아웃</router-link>
+  <div>
   </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { ROUTE_TO} from '@/router/routing';
 export default defineComponent({
   name: 'Landing',
   setup() {
+    ROUTE_TO.HOME();
     return {};
   },
 });
