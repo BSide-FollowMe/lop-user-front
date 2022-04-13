b8e15f25641c52ca21c39075cc49d7c198b0553d
korkt-kim <gnscott6@naver.com>
2022-04-13 19:06:14 +0900

    chore: set default meta tags
    - vue-meta 설치
    - App.vue, Home.vue에 default meta tag 설정

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
+  <metainfo />
   <LayoutController />
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue';
-import LayoutController from '@/components/layouts/LayoutController.vue';
+import setMeta from "@/utils/setMeta";
+import { defineComponent } from "vue";
+import LayoutController from "@/components/layouts/LayoutController.vue";
 export default defineComponent({
+  setup() {
+    setMeta({
+      title: "식물의언어",
+      description: "식집사를 위한 식물 정보 플랫폼, 식물의언어를 찾아오세요",
+      keywords: "식물의언어, 식물의언어, 식물, plantslang",
+      path: "",
+    });
+  },
   components: {
     LayoutController,
   },
